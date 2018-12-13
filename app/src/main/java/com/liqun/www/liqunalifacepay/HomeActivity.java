22f0dc4eea5ebe0bff551dd0ffa96441b1e75fc0
MoeRookie <MoeRookie@163.com>
2018-12-13 10:35:33 +0800

    添加头条屏幕适配方案并去掉应用标题栏

--- app/src/main/java/com/liqun/www/liqunalifacepay/HomeActivity.java
+++ app/src/main/java/com/liqun/www/liqunalifacepay/HomeActivity.java
@@ -1,11 +1,12 @@
 package com.liqun.www.liqunalifacepay;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private static final String TAG = "HomeActivity";
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
