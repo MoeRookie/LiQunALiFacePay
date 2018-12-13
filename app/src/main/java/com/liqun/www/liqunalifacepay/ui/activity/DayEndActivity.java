abac42bef74a60e4ba5400dedf7f134fe0a6fec3
MoeRookie <MoeRookie@163.com>
2018-12-13 17:35:35 +0800

    初始化日结界面

---
+++ app/src/main/java/com/liqun/www/liqunalifacepay/ui/activity/DayEndActivity.java
@@ -0,0 +1,41 @@
+package com.liqun.www.liqunalifacepay.ui.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.liqun.www.liqunalifacepay.R;
+
+public class DayEndActivity extends AppCompatActivity {
+    private ImageView mIvBack;
+
+    public static Intent newIntent(Context packageContext) {
+        Intent intent = new Intent(packageContext, DayEndActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_end);
+        initUI();
+        initListener();
+    }
+
+    private void initListener() {
+        mIvBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void initUI() {
+        mIvBack = findViewById(R.id.iv_back);
+    }
+}
--- app/src/main/java/com/liqun/www/liqunalifacepay/ui/activity/HomeActivity.java
+++ app/src/main/java/com/liqun/www/liqunalifacepay/ui/activity/HomeActivity.java
@@ -1,11 +1,16 @@
-package com.liqun.www.liqunalifacepay;
+package com.liqun.www.liqunalifacepay.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout.LayoutParams;
+
+import com.liqun.www.liqunalifacepay.R;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
@@ -13,6 +18,8 @@ public class HomeActivity extends AppCompatActivity {
     private ImageView mIvHead;
     private Button mBtnVip;
     private Button mBtnNoVip;
+    private long[] mHits = new long[5]; // 设置多击时需要的点击次数
+    private ImageView mIvLiQun;
 
     @Override
 
@@ -20,6 +27,22 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         initUI();
+        initListener();
+    }
+
+    private void initListener() {
+        mIvLiQun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
+                mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+                if (mHits[mHits.length - 1] - mHits[0] < 1000) {
+                    // 满足"五击事件"后,跳转"日结"界面
+                    Intent intent = DayEndActivity.newIntent(HomeActivity.this);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     private void initUI() {
@@ -27,6 +50,7 @@ public class HomeActivity extends AppCompatActivity {
         // 适配home界面的head图片
         mIvHead = findViewById(R.id.iv_head);
         setImageDisplay(mIvHead,898,1920);
+        mIvLiQun = findViewById(R.id.iv_liqun);
     }
 
     /**
